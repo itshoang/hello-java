aee788ce8ed63a80607cc319ecaadd16f400d658
itshoang <89496515+itshoang@users.noreply.github.com>
2021-08-24 22:01:59 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*********************************************************************
+* Java Lab 1
+**********************************************************************
+* Hoang Le
+* 8/24/21
+* CMSC255, Section 4
+*********************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Hoang Le>");
+		System.out.println("<Java Lab 1>");
+		System.out.println("<Creating and running java program>");
+		System.out.println("<8/24/21>");
+		System.out.println("<CSMSC255, Section 4>");
+		System.out.println();
+
+	}
+}
